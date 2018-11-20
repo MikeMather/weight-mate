23e0819bab17521802cfd9328d1cef7166a8f832
mikemather <michael.mather@fytics.com>
2018-11-20 15:50:50 -0330

    Add frontend

---
+++ backend/api/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.conf import settings
+import logging
+import os
+
+def index(request, *args):
+    print(os.path.join(settings.FRONTEND_DIR, 'build', 'index.html'))
+    try:
+        with open(os.path.join(settings.FRONTEND_DIR, 'build', 'index.html')) as f:
+            return HttpResponse(f.read())
+    except FileNotFoundError:
+        logging.exception("Production build of app not found")
+        return HttpResponse(
+            """
+            THIS URL is only used when you have build the production version of the app. Visit http://localhost:3000/ instead
+            or build by running 'yarn run build' to test the production version
+            """,
+            status=501
+        )
+
+def test(request, *args):
+    return JsonResponse({"msg": "This is the result of a connection between client/server"})
